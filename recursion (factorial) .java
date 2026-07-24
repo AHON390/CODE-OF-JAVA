e90a27105f6af34bccfcc7d00723e1706bd56e7c
DARK_SMILE__(_AHON_)__ <dark201100000@gmail.com>
2026-07-24 15:47:39 +0600

    Create recursion (factorial) .java

---
+++ recursion (factorial) .java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Factorial{
+    
+    public static void main(String[] args){
+        
+        Scanner scn=new Scanner(System.in);
+        
+        System.out.print("enter your number :");
+        
+        int n = scn.nextInt();
+        
+        System.out.println("factorial of "+ n + "is:"+factorial(n));
+        
+        
+        
+    }
+    public static int factorial(int n){
+        
+        if(n==0 || n==1){
+            
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+}
